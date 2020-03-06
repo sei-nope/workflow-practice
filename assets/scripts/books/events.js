ed4221693eb688b6224633475ff7155fd538d313
jingjielim <jingjie3000@gmail.com>
2020-03-06 15:58:43 -0500

    Add ajax to get all books
    Add api.js for ajax call to get all books
    Add events.js for event handlers
    Add eventlistener to app.js
    Changed production URL in config.js

---
+++ assets/scripts/books/events.js
@@ -0,0 +1,12 @@
+const api = require('./api')
+const ui = require('./ui')
+
+const onGetBooks = function (event) {
+  event.preventDefault()
+
+  api.getBooks()
+    .then(ui.getBooksSuccessful)
+    .catch(ui.getBooksFailure)
+}
+
+module.exports = onGetBooks
